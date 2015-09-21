84fdaaca5f05cead96d752a23a8476e76fc89b24
cathyyoung <youngcat@uk.ibm.com>
2015-09-21 22:51:41 +0100

    Simple POST method and unit test

--- phonebook.py
+++ phonebook.py
@@ -1,13 +1,30 @@
 import web
 import json
 
-urls = ("/", "hello")
+db = web.database(dbn="sqlite", db="phonebook.db")
+
+urls = ("/", "Phonebook")
 app = web.application(urls, globals())
 
-class hello:
+class Phonebook:
+    
     def GET(self):
-        return "Hello, world!"
+        return "This will list all entries"
 
+    def POST(self):
+        data = json.loads(web.data())
+        
+        fn = data['firstname']
+        sn = data['surname']
+        nm = data['number']
+        if data.has_key('address'):
+            addr = data['address']
+        n = db.insert('phonebook',
+                      firstname=fn,
+                      surname=sn,
+                      number=nm)
+        return "Successfully added %s %s" % (fn, sn)
+    
 
 if __name__ == "__main__":
     app.run()
--- test_phonebook.py
+++ test_phonebook.py
@@ -1,17 +1,30 @@
 import phonebook
+import web
 import unittest
 
-# initialize web server... or assume it's initialized
-# send requests
-# verify return
+# Override phonebook DB with our test DB
+phonebook.db = web.database(dbn="sqlite", db="test_phonebook.db")
+db = phonebook.db
 
-# send POST
-# GET /
-# POST
-# GET /
-# DELETE
-# GET /
-# UPDATE
-# GET /
+class TestPhonebook(unittest.TestCase):
+    
+    def setUp(self):
+        # Reset database per test
+        db.query('DELETE FROM phonebook')
 
-# Errors
+    
+    def test_add_no_address(self):
+        num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+        
+        response = phonebook.app.request("/",
+                                         method='POST',
+                                         data='{"surname":"Mouse",\
+                                         "firstname":"Mickey",\
+                                         "number":"01234567789"}')
+        # Simple check; did we add one row
+        new_num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+        self.assertEqual(new_num_rows, num_rows+1)
+        
+
+if __name__ == '__main__':
+    unittest.main()
