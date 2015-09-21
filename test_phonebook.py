5f1b33ddf11814eeb63f2b6a53d4936926569bcb
cathyyoung <youngcat@uk.ibm.com>
2015-09-21 17:16:33 +0100

    Initial commit, hello world

---
+++ test_phonebook.py
@@ -0,0 +1,17 @@
+import phonebook
+import unittest
+
+# initialize web server... or assume it's initialized
+# send requests
+# verify return
+
+# send POST
+# GET /
+# POST
+# GET /
+# DELETE
+# GET /
+# UPDATE
+# GET /
+
+# Errors
