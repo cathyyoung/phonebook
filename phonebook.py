c63d6703a5eccb8197e43809699f2345ab2dee9c
cathyyoung <youngcat@uk.ibm.com>
2015-09-22 15:21:35 +0100

    Check for required attributes in POST (create) Add method and tests for GET (list all)

--- phonebook.py
+++ phonebook.py
@@ -9,22 +9,38 @@ app = web.application(urls, globals())
 class Phonebook:
     
     def GET(self):
-        return "This will list all entries"
+        results = []
+        q = db.query("SELECT firstname, surname, number, address FROM phonebook")
+        for row in q:
+            results.append({'firstname':row.firstname,
+                            'surname':row.surname,
+                            'number':row.number,
+                            'address':row.address})
+        json_response = json.dumps(results)
+        return json_response
 
     def POST(self):
         data = json.loads(web.data())
+
+        # Make sure all required attributes are present
+        for required_attr in ('firstname','surname','number'):
+            if not data.has_key(required_attr):
+                raise web.badrequest("Attributes firstname, surname, and number must be present")
         
         fn = data['firstname']
         sn = data['surname']
         nm = data['number']
-        if data.has_key('address'):
-            addr = data['address']
+        # Check to see whether address is present, or None if not
+        addr = data.has_key('address') and data['address'] or None
+        
         n = db.insert('phonebook',
                       firstname=fn,
                       surname=sn,
-                      number=nm)
-        return "Successfully added %s %s" % (fn, sn)
-    
+                      number=nm,
+                      address=addr)
+        # Return 201 Created
+        return web.created("Successfully added %s %s" % (fn, sn))
+
 
 if __name__ == "__main__":
     app.run()
--- test_phonebook.py
+++ test_phonebook.py
@@ -1,6 +1,5 @@
 import phonebook
-import web
-import unittest
+import web, unittest, json
 
 # Override phonebook DB with our test DB
 phonebook.db = web.database(dbn="sqlite", db="test_phonebook.db")
@@ -13,17 +12,129 @@ class TestPhonebook(unittest.TestCase):
         db.query('DELETE FROM phonebook')
 
     
-    def test_add_no_address(self):
-        num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+    def test_create(self):
+        '''Test that a POST request with phonebook data results in the entry being added to the phonebook'''
         
-        response = phonebook.app.request("/",
-                                         method='POST',
-                                         data='{"surname":"Mouse",\
-                                         "firstname":"Mickey",\
-                                         "number":"01234567789"}')
+        td1 = '{"surname":"Mouse",\
+                 "firstname":"Mickey",\
+                 "number":"01234567789"}'
+        
+        response = phonebook.app.request("/", method='POST', data=td1)
         # Simple check; did we add one row
+        num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+        self.assertEqual(num_rows, 1)
+
+        td2 = '{"surname":"Mouse",\
+                 "firstname":"Minnie",\
+                 "number":"02045679920",\
+                 "address":"12 New Road, Disneyland"}'
+        
+        response = phonebook.app.request("/", method='POST', data=td2)
+        # Check HTTP response
+        self.assertEqual(response.status, "201 Created")
+
         new_num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
-        self.assertEqual(new_num_rows, num_rows+1)
+
+        # assert 2 mice, one mickey, one minnie, one address,
+        mickey = db.query('''SELECT firstname, surname, number, address FROM phonebook
+                            WHERE surname = "Mouse" AND firstname = "Mickey"''')
+        mickey = mickey[0]
+        self.assertEqual(mickey.firstname, "Mickey")
+        self.assertEqual(mickey.surname, "Mouse")
+        self.assertEqual(mickey.number, "01234567789")
+        self.assertEqual(mickey.address, None)
+
+        minnie = db.query('''SELECT firstname, surname, number, address FROM phonebook
+                            WHERE surname = "Mouse" AND firstname = "Minnie"''')
+        minnie = minnie[0]
+        self.assertEqual(minnie.firstname, "Minnie")
+        self.assertEqual(minnie.surname, "Mouse")
+        self.assertEqual(minnie.number, "02045679920")
+        self.assertEqual(minnie.address, "12 New Road, Disneyland")
+
+    def test_create_missing_fields(self):
+        '''Test that errors are thrown and appropriate HTTP status code
+        is returned when a required attribute is missing in POST (create) request.
+        Test that a missing address attribute (optional) is accepted and entry is created.'''
+        
+        no_name_data = '{"firstname":"Minnie",\
+                 "number":"02045679920",\
+                 "address":"12 New Road, Disneyland"}'
+        
+        response = phonebook.app.request("/", method='POST', data=no_name_data)
+
+        # Check appropriate response status
+        self.assertEqual(response.status, "400 Bad Request")
+
+        # Check nothing's been added to the database
+        num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+        self.assertEqual(num_rows, 0)
+
+        no_num_data = '{"surname":"Mouse",\
+                 "firstname":"Minnie",\
+                 "address":"12 New Road, Disneyland"}'
+
+        response = phonebook.app.request("/", method='POST', data=no_num_data)
+        self.assertEqual(response.status, "400 Bad Request")
+        num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+        self.assertEqual(num_rows, 0)
+
+        no_addr_data = '{"surname":"Mouse",\
+                 "firstname":"Mickey",\
+                 "number":"01234567789"}'
+        
+        response = phonebook.app.request("/", method='POST', data=no_addr_data)
+        # Check 1 row added
+        num_rows = db.query("SELECT COUNT(*) AS entries FROM phonebook")[0].entries
+        self.assertEqual(num_rows, 1)
+
+    def test_list_all_empty(self):
+        '''Test the response for an empty phonebook'''
+        
+        response = phonebook.app.request("/", method='GET')
+        
+        # Fails if invalid json
+        json_resp = self.json_data(response.data)
+
+        self.assertEqual(len(json_resp), 0)
+
+    def test_list_all(self):
+        '''Test that all entries are returned'''
+        
+        data = ['{"surname":"Mouse","firstname":"Mickey","number":"01234567789"}',
+                '{"surname":"Mouse","firstname":"Minnie","number":"02045679920","address":"12 New Road, Disneyland"}',
+                '{"surname":"Duck","firstname":"Donald","number":"028384752","address":"123a Main Street, Disneyland"}',
+                '{"surname":"Duck","firstname":"Daisy","number":"028384752","address":"123a Main Street, Disneyland"}']
+
+        # add data to phonebook
+        for entry in data:
+            response = phonebook.app.request("/", method='POST', data=entry)
+
+        # Check they've been added to database
+        entries = db.query("SELECT * FROM phonebook")
+        self.assertEqual(len(list(entries)), 4)
+
+        # Test response
+        response = phonebook.app.request("/", method='GET')
+        self.assertEqual(response.status, "200 OK")
+
+        # Fails if invalid json
+        json_resp = self.json_data(response.data)
+
+        self.assertEqual(len(json_resp), 4)
+        
+
+## Utility methods ##
+#####################
+
+    def json_data(self, response_data):
+        try:
+            json_resp = json.loads(response_data)
+            return json_resp
+        except ValueError:
+            # not valid json
+            self.fail("Received invalid JSON response")
+       
         
 
 if __name__ == '__main__':
